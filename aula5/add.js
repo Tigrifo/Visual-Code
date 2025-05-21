15f6b72823aad36ab198bf00d1ec115386a7e401
Tigrifo <saacbar@gmail.com>
2025-05-21 14:23:27 -0300

    Delete aula5 directory

--- aula5/add.js
+++
@@ -1,8 +0,0 @@
-function funtion(){
-    document.querySelector('#div').classList.add("classe1")
-    document.querySelector('#cinza').classList.remove("cinza")
-}
-function removation(){
-    document.querySelector('#div').classList.remove("classe1")
-    document.querySelector('#div').classList.add("cinza")
-}
--- aula5/classlist.js
+++
@@ -1,3 +0,0 @@
-function minhaFuncao(){
-    document.querySelector('#minhaDiv').classList.add("classe1","classe2","classe3")
-}
