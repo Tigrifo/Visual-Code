0d03da1f1312cd2dc6b60d7c20fb8235bd5f161d
Tigrifo <saacbar@gmail.com>
2025-06-02 16:26:54 -0300

    Add files via upload

---
+++ JavaScript/contador de cliques/cont.js
@@ -0,0 +1,19 @@
+const t1 = document.querySelector("#T1")
+const t2 = document.querySelector("#T2")
+const t3 = document.querySelector("#T3")
+
+let a1 = 0
+let a2 = 0
+let a3 = 0
+function texto1(){
+    a1++
+    t1.innerText = a1
+}
+function texto2(){
+    a2++
+    t2.innerText = a2
+}
+function texto3(){
+    a3++
+    t3.innerText = a3
+}
