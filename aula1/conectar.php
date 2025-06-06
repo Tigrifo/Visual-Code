9822cbbb80a4d593f0d05e02933ed6d4f5caf5be
Tigrifo <saacbar@gmail.com>
2025-06-06 16:31:09 -0300

    Add files via upload

---
+++ aula1/conectar.php
@@ -0,0 +1,10 @@
+﻿<?php
+$host = "localhost";
+$usuario = "root";
+$senha = ""; // senha padrao do WAMP/XAMPP
+$banco = "cursos_db";
+$conn = new mysqli($host, $usuario, $senha, $banco);
+if ($conn->connect_error) {
+ die("Falha na conexao: " . $conn->connect_error);
+}
+?>
