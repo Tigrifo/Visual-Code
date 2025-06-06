37f3b4dc80dc62792309848481c986cbdd4f4862
Tigrifo <saacbar@gmail.com>
2025-06-06 16:30:16 -0300

    Add files via upload

---
+++ php&front/aula1/processar.php
@@ -0,0 +1,21 @@
+﻿<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    include("conectar.ph");
+    
+    $nome = $conn->real_escape_string($_POST['nome']);
+    $carga_horaria = (int) $_POST['carga_horaria'];
+    $descricao = $conn->real_escape_string($_POST['descricao']);
+
+    $sql = "INSERT INTO cursos (nome, carga_horaria, descricao) VALUES ('$nome', $carga_horaria, '$descricao')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Curso cadastrado com sucesso!";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+
+    $conn->close();
+} else {
+    echo "Acesso inválido!";
+}
+?>
